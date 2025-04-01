3457d76c069656f287f3b9cbbdd6eb94a255334d
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-01 12:38:08 -0400

    puppy id shows when clicking name

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { useState } from 'react'
 import './App.css';
 import {puppyList} from './data.js';
 
 function App() {
-  // const [count, setCount] = useState(0)
-  console.log("puppy list: ",puppyList);
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log("puppies from useState: ",puppies);
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  // console.log("puppyList: ", puppyList);
+  function handleClick() {
+    
+  }
+
   return (
-    <>
-
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-    </>
-  )
+    <div className="App">
+      <p>{featPupId}</p>
+      {/* { featuPupID && <p>{ featPupId }</p> } */}
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
-
 export default App
